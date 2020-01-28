4608e10c4d3ac1f83a53f001ecfa0ede822c45f8
Farkass47 <sandor.farkas@t-online.hu>
2020-01-28 18:24:33 +0100

    Updates

---
+++ precache-manifest.7db99832b76c4d23947a769201b31e42.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "681b12c8c27730b78df5e838e99b85ca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "619988dd8e3d94e4c401",
+    "url": "/static/css/main.b92d5f95.chunk.css"
+  },
+  {
+    "revision": "ded73dc3830fb2d80c34",
+    "url": "/static/js/2.b7dd99f8.chunk.js"
+  },
+  {
+    "revision": "619988dd8e3d94e4c401",
+    "url": "/static/js/main.2e1705a8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "db9097e874e68b7ed7115ff05ffcb880",
+    "url": "/static/media/0.db9097e8.jpg"
+  },
+  {
+    "revision": "fbff4784004cc97463c883477a0090d1",
+    "url": "/static/media/1.fbff4784.jpg"
+  },
+  {
+    "revision": "c4ea9528b67823bd94c90de3d97d75a3",
+    "url": "/static/media/2.c4ea9528.jpg"
+  },
+  {
+    "revision": "62a781c89c590abb27129638d9d18c4d",
+    "url": "/static/media/3.62a781c8.jpg"
+  },
+  {
+    "revision": "0f8ca81ee9f7b2bc27102267f9e23864",
+    "url": "/static/media/4.0f8ca81e.jpg"
+  },
+  {
+    "revision": "1b557aa7ccfa88b6b3f4723c27d3f2d6",
+    "url": "/static/media/5.1b557aa7.jpg"
+  },
+  {
+    "revision": "c80394c2a96ffad09a9df4e4815745f2",
+    "url": "/static/media/6.c80394c2.jpg"
+  }
+]);
